72dc059be9e52832efc6990a27a304816a5d7e64
Thamy <thamiris_oliveira2@hotmail.com>
2023-09-19 13:08:24 -0300

    controlador aula add

--- src/bancoDeDados.js
+++ src/bancoDeDados.js
@@ -3,7 +3,19 @@ const bancodedados = {
     instrutores: [
         {id: 1, nome: "Guido", email: "guido@emaiil.com", status: true},
         {id: 2, nome: "Dani", email: "danio@emaiil.com", status: true}
+    ],
+
+    aulas: [
+        {
+            id: 1,
+            instrutor_id: 1,
+            titulo: 'Primeiro Servidor',
+            descricao: 'Construindo o primeiro servidor'
+        }
     ]
-};
+}
+
 
 module.exports = bancodedados
+
+atualizaçÃo
